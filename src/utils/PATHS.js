8a88d363e21067d7f331c3a26947e59cff231dfc
Laiba_012 <laibahameed012@gmail.com>
2025-01-23 15:47:21 +0500

    Refactor: PUBLIC_ROUTES in utils/path.js file

--- src/utils/PATHS.js
+++ src/utils/PATHS.js
@@ -1,10 +1,17 @@
-export const ROOT_ROUTE = "/"
-const NAV_ROUTES = {
-    home: { value: 'home', label: 'HOME', path: '/' },
-    store: { value: 'store', label: 'STORE', path: '/store' },
-    about: { value: 'about', label: 'ABOUT', path: '/about' },
-    blog: { value: 'blog', label: 'BLOG', path: '/blog' },
-    contact: { value: 'contact', label: 'CONTACT', path: '/contact' },
+export const ROOT_ROUTE = "/";
+
+export const PUBLIC_ROUTES = {
+    home: ROOT_ROUTE,
+    store: "/store",
+    about: "/about",
+    contact: "/contact",
 };
 
-export const NAV_LIST = Object.values(NAV_ROUTES)
+const NAV_ROUTES = {
+    home: { value: "home", label: "HOME", path: PUBLIC_ROUTES.home },
+    store: { value: "store", label: "STORE", path: PUBLIC_ROUTES.store },
+    about: { value: "about", label: "ABOUT", path: PUBLIC_ROUTES.about },
+    contact: { value: "contact", label: "CONTACT", path: PUBLIC_ROUTES.contact },
+};
+
+export const NAV_LIST = Object.values(NAV_ROUTES);
