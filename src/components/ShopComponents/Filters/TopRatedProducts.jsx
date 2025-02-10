4036e49d3877354e6e01513915d8dd4f2560c6cd
Laiba_012 <laibahameed012@gmail.com>
2025-02-10 16:50:39 +0500

    fix product id issue

--- src/components/ShopComponents/Filters/TopRatedProducts.jsx
+++ src/components/ShopComponents/Filters/TopRatedProducts.jsx
@@ -4,23 +4,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-
 const TopRatedProducts = () => {
   return (
     <div className="py-4 bg-white w-full">
       <h3 className="font-semibold mb-6 text-secondary uppercase">Top Rated </h3>
       
-      <Link href={`/products/${product.id}`} className="space-y-4 cursor-pointer">
-        {BEST_SELLER.map((product) => (
-          <div key={product.id} className="flex items-start gap-4 mb-2 hover:shadow-xl animate">
-            <Image width={75} height={75} src={product.image} alt={product.title} className=" object-cover " />
+      {BEST_SELLER.map((product) => (
+        <Link key={product.id} href={`/products/${product.id}`} className="space-y-4 cursor-pointer">
+          <div className="flex items-start gap-4 mb-2 hover:shadow-xl animate">
+            <Image width={75} height={75} src={product.image} alt={product.title} className="object-cover" />
             <div>
               <h4 className="text-sm font-light text-secondary tracking-wider">{product.title}</h4>
-              <p className=" text-secondary font-semibold py-2 font-serif">{product.price}.00 $</p>
+              <p className="text-secondary font-semibold py-2 font-serif">{product.price}.00 $</p>
             </div>
           </div>
-        ))}
-      </Link>
+        </Link>
+      ))}
     </div>
   );
 };
