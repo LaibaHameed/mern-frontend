ad98b58e1ec51016b5ac2f81187747ebca806e4b
Laiba_012 <laibahameed012@gmail.com>
2025-02-16 10:27:09 +0500

    Cart page responsive view for small devices

--- src/components/CartPage/page.jsx
+++ src/components/CartPage/page.jsx
@@ -1,9 +1,8 @@
 'use client'
 import React, { useState } from 'react';
 import Container from '../shared/Container';
-import Image from 'next/image';
-import { BsTrash3 } from 'react-icons/bs';
 import { CART_ITEMS } from '@/constants/general';
+import CartTable from './CartTable';
 
 const CartPage = () => {
     const [cartItems, setCartItems] = useState(CART_ITEMS);
@@ -19,49 +18,7 @@ const CartPage = () => {
     return (
         <div className='flex-center sm:m-12 mx-6'>
             <Container>
-                <div className='overflow-x-auto'>
-                    <table className='min-w-full'>
-                        <thead>
-                            <tr>
-                                <td className='px-2 py-6 border-b border-gray-300 uppercase font-semibold text-left'>Product</td>
-                                <td className='px-2 py-6 border-b border-gray-300 uppercase font-semibold text-left'>Price</td>
-                                <td className='px-2 py-6 border-b border-gray-300 uppercase font-semibold text-left'>Quantity</td>
-                                <td className='px-2 py-6 border-b border-gray-300 uppercase font-semibold text-left'>Total</td>
-                                <td className='px-2 py-6 border-b border-gray-300 uppercase font-semibold text-left'></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {cartItems.map((item) => (
-                                <tr key={item.id} className='border-b border-gray-300'>
-                                    <td className='px-2 py-6 min-w-[300px] flex items-center gap-2 text-left cursor-pointer'>
-                                        <Image src={item.image} alt={item.name} width={70} height={70} />
-                                        <p className='text-gray-600 font-light hover:text-primary animate'>{item.name}</p>
-                                    </td>
-                                    <td className='px-2 py-6 text-left font-semibold tracking-wider'>${item.price}.00</td>
-                                    <td className='px-2 py-6 text-left'>
-                                        <div className='flex items-center border border-gray-300 w-fit'>
-                                            <button
-                                                onClick={() => updateQuantity(item.id, -1)}
-                                                className='py-2 border-r w-12 border-gray-300 cursor-pointer'>-</button>
-                                            <span className='py-2 w-12 flex-center'>{item.quantity}</span>
-                                            <button
-                                                onClick={() => updateQuantity(item.id, 1)}
-                                                className='py-2 border-l w-12 border-gray-300 cursor-pointer'>+</button>
-                                        </div>
-                                    </td>
-                                    <td className='px-2 py-6 text-left font-semibold tracking-wider min-w-[100px]'>
-                                        ${item.price * item.quantity}.00
-                                    </td>
-                                    <td className='px-2 py-6 text-left'>
-                                        <button className='hover:text-red-700 font-extrabold cursor-pointer'>
-                                            <BsTrash3 />
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                <CartTable cartItems={cartItems} updateQuantity={updateQuantity} />
 
                 <div className='my-12'>
                     <h3 className='font-semibold mb-2 uppercase'>Add Note</h3>
