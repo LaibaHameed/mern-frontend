bcb797095d673c71dbbdf160f679446249e883a9
hamza7681 <hamzambf@gmail.com>
2025-02-19 21:42:50 +0500

    Redux Implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,25 @@
-import { Inter } from "next/font/google";
+import {Inter} from 'next/font/google';
 import Header from '@/components/layouts/header';
 import Footer from '@/components/layouts/footer';
-import "./globals.css";
+import './globals.css';
+import ReduxProvider from '@/redux/provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ['latin']});
 
 export const metadata = {
-  title: "Botanical",
-  description: "Ecom web app",
+  title: 'Botanical',
+  description: 'Ecom web app',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ReduxProvider>
+          <Header />
+          {children}
+          <Footer />
+        </ReduxProvider>
       </body>
     </html>
   );
---
+++ src/redux/slices/user/userSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+// ----------------------------------------------------------------------
+const defaultState = {
+  currentUser: null,
+};
+
+const slice = createSlice({
+  name: 'users',
+  initialState: defaultState,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    },
+    resetUserState: () => defaultState,
+  },
+});
+
+// Reducer
+export default slice.reducer;
+
+export const actions = slice.actions;
+
+// selectors
+export const getCurrentUser = (state) => state.user.currentUser;
